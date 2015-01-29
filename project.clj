add34d700a1ade1689395260550b061ba2c83fab
Dan Wysocki <dwysocki@oswego.edu>
2015-01-29 14:11:44 -0500

    Updated version number

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject rock-paper-markov "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]]
+(defproject rock-paper-markov "0.1.0"
+  :description "A rock-paper-scissors game against a Markov Chain driven AI."
+  :url "https://github.com/dwysocki/rock-paper-markov"
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.6.0"]]
   :main rock-paper-markov.core)
